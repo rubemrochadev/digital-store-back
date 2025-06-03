e8628157b2014032dada7483434f4eb5f5489f97
rubemrochadev <rubemrocha.dev@gmail.com>
2025-06-03 16:39:52 -0300

    tokenização

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,24 @@
-function rotaProtegida(req, res, next){
-    if(!req.headers['authorization']){
-        res.send({
+const jwt = require("jsonwebtoken");
+
+function rotaProtegida(req, res, next) {
+    const token = req.headers.authorization;
+
+    if (!token){
+        res.status(401).send({
             tipo: "warning",
-            mensagem:"Não autorizado"
+            mensagem: "Não autorizado"
         });
     }
+
+    jwt.verify(token.split(" ")[1], process.env.SEGREDO, (error) => {
+        if (error) {
+            res.status(401).send({
+                tipo: "warning",
+                mensagem: "token inválido"
+            })
+        }
+        next();
+    });
 }
 
 module.exports = {
